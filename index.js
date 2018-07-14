bce499ad3014e9db600182912fc49bf652f33370
Jan Soehnitz <jan.soehnitz@googlemail.com>
2018-07-14 23:37:25 +0200

    adjusted to word on nodejs 9

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ require('./startup/prod')(app);
 require('./startup/routes')(app);
 
 /* start app and server */
+/*
 if (process.env.NODE_ENV !== 'production') {
     const port = process.env.PORT || config.get('server.port');
     app.listen(port, () => winston.info(`Listening on port ${port}...`));
 }
+*/
 
 module.exports  = app;
